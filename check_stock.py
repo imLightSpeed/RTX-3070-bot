f27639de977f51af4dd1a46a0f2e08d878e8b15f
Luke Sanborn <lukesanborn@outlook.com>
2020-11-05 17:16:10 -0500

    more changes

--- check_stock.py
+++ check_stock.py
@@ -1,4 +1,5 @@
 from mail import mail
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import logging
@@ -9,10 +10,16 @@ class best_buy:
         self.item  = item
         LOGGER.setLevel(logging.WARNING)
         options = Options()
-        options.add_argument("--log-level=3")
         options.headless = True
-        options.binary_location = GOOGLE_CHROME_PATH
-        driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+        #Bellow is for heroku
+        options.add_argument("--log-level=3")
+        options.add_argument("-- disable-gpu")
+        options.add_argument("-- no-sandbox")
+        CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+        chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+        # PATH = "chromedriver.exe"
+        # driver = webdriver.Chrome(PATH, options=options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
         try:
             driver.get(url)
             is_instock =  driver.find_element_by_class_name('add-to-cart-button').text
@@ -27,11 +34,18 @@ class best_buy:
 class newegg:
     def check_stock(self, item, url):
         self.item  = item
+        LOGGER.setLevel(logging.WARNING)
         options = Options()
         options.headless = True
+        #Bellow is for heroku
         options.add_argument("--log-level=3")
-        options.binary_location = GOOGLE_CHROME_PATH
-        driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+        options.add_argument("-- disable-gpu")
+        options.add_argument("-- no-sandbox")
+        CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+        chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+        # PATH = "chromedriver.exe"
+        # driver = webdriver.Chrome(PATH, options=options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
         try:
             driver.get(url)
             is_instock =  driver.find_element_by_class_name('product-buy').text
