1fc55fd69e3bb4c5e5d80869645c227c392bda0e
Luke Sanborn <lukesanborn@outlook.com>
2020-11-05 17:01:57 -0500

    chromedriver path

--- check_stock.py
+++ check_stock.py
@@ -11,8 +11,8 @@ class best_buy:
         options = Options()
         options.add_argument("--log-level=3")
         options.headless = True
-        PATH = "chromedriver.exe"
-        driver = webdriver.Chrome(PATH, options=options)
+        options.binary_location = GOOGLE_CHROME_PATH
+        driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
         try:
             driver.get(url)
             is_instock =  driver.find_element_by_class_name('add-to-cart-button').text
@@ -30,8 +30,8 @@ class newegg:
         options = Options()
         options.headless = True
         options.add_argument("--log-level=3")
-        PATH = "chromedriver.exe"
-        driver = webdriver.Chrome(PATH, options=options)
+        options.binary_location = GOOGLE_CHROME_PATH
+        driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
         try:
             driver.get(url)
             is_instock =  driver.find_element_by_class_name('product-buy').text
